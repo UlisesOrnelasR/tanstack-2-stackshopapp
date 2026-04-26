718ad00dce625f7b755fd9e6767ab69ab1260091
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2026-04-26 15:09:52 -0600

    chore: Change talwindcss to shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { Button } from "#/components/ui/button";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-	return <div>main page</div>;
+	return (
+		<div>
+			main page
+			<Link to="/products">
+				<Button>Products</Button>
+			</Link>
+		</div>
+	);
 }
