a27ad4f7aaee04f1e6dca356a4e4f062184cd7d2
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2026-05-09 14:39:38 -0600

    chore: base form

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,11 +1,11 @@
 import { createServerFn } from "@tanstack/react-start";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
-import { db } from "@/db";
-import { products } from "@/db/schema";
+import { type ProductInsert, type ProductSelect, products } from "@/db/schema";
 
 export const getAllProducts = createServerFn({ method: "GET" }).handler(
 	async () => {
+		const { db } = await import("@/db");
 		try {
 			const allProducts = await db.select().from(products);
 			return allProducts;
@@ -18,8 +18,8 @@ export const getAllProducts = createServerFn({ method: "GET" }).handler(
 
 export const getRecommendedProducts = createServerFn({ method: "GET" }).handler(
 	async () => {
+		const { db } = await import("@/db");
 		try {
-			// await new Promise((resolve) => setTimeout(resolve, 2000)); this was just to show how Suspense and use works
 			const recommendedProducts = await db.select().from(products).limit(3);
 			return recommendedProducts;
 		} catch (error) {
@@ -34,6 +34,7 @@ const idSchema = z.string();
 export const getProductById = createServerFn({ method: "GET" })
 	.inputValidator((id: string) => id)
 	.handler(async ({ data }) => {
+		const { db } = await import("@/db");
 		const id = idSchema.parse(data);
 
 		const product = await db
@@ -44,3 +45,23 @@ export const getProductById = createServerFn({ method: "GET" })
 
 		return product[0] ?? null;
 	});
+
+export async function createProduct(
+	data: ProductInsert,
+): Promise<ProductSelect> {
+	const { db } = await import("@/db");
+	try {
+		const result = await db.insert(products).values(data).returning();
+		const product = result[0];
+		if (!product) {
+			throw new Error(
+				"Failed to create product: no product returned from database",
+			);
+		}
+
+		return product;
+	} catch (error) {
+		console.error("Error creating product", error);
+		throw error;
+	}
+}
